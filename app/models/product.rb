e192f3a3bfc475e966e26da8fe925bdd588d2fdc
Jim Van Fleet <jim@jimvanfleet.com>
2013-11-09 14:32:26 -0500

    Specs passing

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,11 @@ class Product < ActiveRecord::Base
   has_many :communities, through: :product_offerings
 
   def self.in_user_communities(user)
-    []
+    # Ideally this would work, but Rails seems to complain.  2phase it.
+    # includes(:communities).where(community: {id: user.communities})
+    #
+    community_ids = user.communities.pluck(:id)
+    includes(:communities).where(communities: {id: community_ids})
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :memberships
-  has_many :communitites, through: :memberships
+  has_many :communities, through: :memberships
 end
