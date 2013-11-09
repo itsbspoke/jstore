779989bf2e87025599a72daab27c84a844ee8c0a
Jim Van Fleet <jim@jimvanfleet.com>
2013-11-09 14:26:21 -0500

    Adding a failing test case

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ActiveRecord::Base
 
   has_many :product_offerings
-  has_many :memberships, through: :product_offerings
+  #has_many :memberships, through: :product_offerings
+  has_many :communities, through: :product_offerings
+
+  def self.in_user_communities(user)
+    []
+  end
 
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "user community finder" do
+
+    before(:each) do
+      @po1 = create(:product_offering)
+      @po2 = create(:product_offering)
+      @shopper = create(:user)
+      @shopper_membership = create(:membership, user: @shopper, community: @po1.community)
+    end
+    let(:products){  Product.in_user_communities(@shopper) }
+    it "should find a product in users communities" do
+      products.should include(@po1.product)
+    end
+    it "should not include a product outside the users community" do
+      products.should_not include(@po2.product)
+    end
+  end
 end
